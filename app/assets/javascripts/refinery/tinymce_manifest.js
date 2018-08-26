aaa3ff2bfc57c120d17cbdb7c37ec78f15bc4e6a
authentic <galaxy73rambler@yahoo.com>
2018-08-26 16:09:39 -0400

    editor setup

--- app/assets/javascripts/refinery/tinymce_manifest.js
+++ app/assets/javascripts/refinery/tinymce_manifest.js
@@ -19,6 +19,10 @@
  *= require tinymce/plugins/autolink/plugin
  *= require tinymce/plugins/autoresize/plugin
  *= require tinymce/plugins/wordcount/plugin
+ *= require tinymce/plugins/contextmenu/plugin
+ *= require tinymce/plugins/bbcode/plugin
+ *= require tinymce/plugins/charmap/plugin
+ *= require tinymce/plugins/colorpicker/plugin
  *= require refinery/boot_tinymce
  *= require_self
 */
