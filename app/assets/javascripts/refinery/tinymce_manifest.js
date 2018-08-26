38adde003c1dae6114468522f0ebdc94b300c132
authentic <galaxy73rambler@yahoo.com>
2018-08-26 15:50:44 -0400

    editor setup

---
+++ app/assets/javascripts/refinery/tinymce_manifest.js
@@ -0,0 +1,21 @@
+/*
+ * TINYMCE
+ */
+
+/*
+   Namespace: TINYMCE
+   Global Tinymce namespace.
+*/
+/*
+ *= require tinymce/tinymce
+ *= require tinymce/tinymce.jquery
+ *= require tinymce/plugins/link/plugin
+ *= require tinymce/plugins/image/plugin
+ *= require tinymce/plugins/fullscreen/plugin
+ *= require tinymce/plugins/stylebuttons/plugin
+ *= require tinymce/plugins/code/plugin
+ *= require tinymce/plugins/advlist/plugin
+ *= require tinymce/plugins/anchor/plugin
+ *= require refinery/boot_tinymce
+ *= require_self
+*/
